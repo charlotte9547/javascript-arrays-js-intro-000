0d96d071078e9263c19cf89496236dab2b22f4ed
Xingxin Chen <xingxinc@andrew.cmu.edu>
2018-04-08 21:48:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,5 +28,6 @@ function addElementToEndOfArray (array, element) {
   return [...array,element]
 }
 function destructivelyAddElementToEndOfArray (array,element){
-return array =  [...array,element]
+array =  [...array,element]
+return array
 }
